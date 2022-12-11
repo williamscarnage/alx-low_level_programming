e91b29c7306e389801e501986d49b5b0f30aa903
williamscarnage <tagbewilliams4@gmail.com>
2022-12-11 05:09:31 -0800

    numz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	int c;
+	(c = 0);
+	while
+		(c < 10);
+	putchar(c + '0');
+	putchar('\n');
+	return (0);
+}
