f27c9feb60fad2b373664ace0e4ffafaa81d7039
williamscarnage <tagbewilliams4@gmail.com>
2022-12-12 02:04:26 -0800

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,17 +7,18 @@
  */
 
 int main(void)
-{
+	{
 	int a;
 
-	for (a = 0; a < 100; a++) {
+	for (a = 0; a < 10; a++)
+       	{
 	       putchar(a + '0');
-	}
-		if (a < 99) {
-	       putchar(',');
-	       putchar(' ');
+	       if (a < 9)
+	       {
+	               putchar(',');
+	               putchar(' ');
 		}
-
+	}
 	putchar('\n');
 	return 0;
-}
+	}
