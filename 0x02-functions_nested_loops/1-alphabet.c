99b8a37a591afd95903b4f4b964700295c96ba7e
williamscarnage <tagbewilliams4@gmail.com>
2022-12-14 15:27:24 -0800

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void);
+{
+	char bill;
+
+	for (bill = 'a'; bill <= 'z' bill++)
+		_putchar(bill);
+	_putchar('\n');
+	return (0);
+}
