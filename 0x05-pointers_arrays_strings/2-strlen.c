f72059217c1c0fb7cb73e5c71689632bed3ebe3e
williamscarnage <tagbewilliams4@gmail.com>
2022-12-19 15:25:42 -0800

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - return length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int leng = 0;
+
+	while (*s != '\0')
+	{
+		leng++;
+		s++;
+	}
+
+	return (leng);
+}
