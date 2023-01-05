217aa5a0c42ea3d1dade6e379e8523a6b03b8f18
williamscarnage <tagbewilliams4@gmail.com>
2023-01-05 03:27:45 -0800

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - print a string recursively
+ * @s: string parameter
+ */
+
+void _puts_recursion(char *s)
+{
+	s = "lpa\0";
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+}
